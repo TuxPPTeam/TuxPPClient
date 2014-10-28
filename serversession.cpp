3b7b54956bf095c028297ceec5e304701454ab67
lukas-toldy <lukas.toldy@gmail.com>
2014-10-28 21:16:45 +0100

    Pridané ServerSession

---
+++ serversession.cpp
@@ -0,0 +1,5 @@
+#include "serversession.h"
+
+ServerSession::ServerSession()
+{
+}
---
+++ serversession.h
@@ -0,0 +1,10 @@
+#ifndef SERVERSESSION_H
+#define SERVERSESSION_H
+
+class ServerSession
+{
+public:
+    ServerSession();
+};
+
+#endif // SERVERSESSION_H
