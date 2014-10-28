7775bf972648d0d6cb3817a12aa5b47690ad6aff
lukas-toldy <lukas.toldy@gmail.com>
2014-10-28 21:28:59 +0100

    Pridané atribúty a metódy do serversession.h

--- serversession.h
+++ serversession.h
@@ -1,10 +1,37 @@
 #ifndef SERVERSESSION_H
 #define SERVERSESSION_H
 
+class User
+{
+    pk_context key; //public key
+    std::string id;
+
+public:
+    std::string getId();
+    pk_context getKey();
+};
+
 class ServerSession
 {
+    std::string id;
+    std::Vector<User> users;
+    QSslSocket server;
+
+    rsa_context rsa_ctx;
+
 public:
     ServerSession();
+    ~ServerSession();
+
+    bool isConnected();
+
+    QByteArray sendRequest(User user);
+
+    void createUserConnection(User user, QByteArray challenge, pk_context key);
+
+    void sendData(User user, QByteArray data);
+
+    Vector<pk_context> getPublicKeys();
 };
 
 #endif // SERVERSESSION_H
