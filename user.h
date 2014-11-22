5a6456622b34cd8094591f25e41334fb6bea43e2
lukas-toldy <lukas.toldy@gmail.com>
2014-11-23 00:25:17 +0100

    User constructor removed

--- user.h
+++ user.h
@@ -15,7 +15,6 @@ class User : public QObject
 {
     Q_OBJECT
 public:
-    explicit User(QObject *parent = 0);
     explicit User(QObject *parent = 0, QString userName = NULL, QByteArray pubKey = NULL, QHostAddress *host = NULL);
     explicit User(QObject *parent = 0, qint64 = 0, QString userName = NULL, QByteArray pubKey = NULL, QHostAddress *host = NULL);
 
