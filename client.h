6cbb39a32ef62dba9dcca964b9bb82f7e026b09d
lukas-toldy <lukas.toldy@gmail.com>
2014-11-24 23:32:02 +0100

    Because of conflict

--- client.h
+++ client.h
@@ -4,14 +4,16 @@
 #include "user.h"
 
 #include <QObject>
-#include <QTcpSocket>
+#include <QSslSocket>
+#include <QUdpSocket>
 #include <QHostAddress>
 
-static const int PORT = 1234;
+static const int SERVER_PORT = 1234;
+static const int CLIENT_PORT = 4321;
 static const QHostAddress serverAddress = QHostAddress::LocalHostIPv6;
-static const char commandDelimiter = '\0';
+static const char commandDelimiter = '\31';
 
-enum Command { ECHO, LOGIN, LOGOUT, REGISTER, GETUSERS};
+enum Command { ECHO, LOGIN, LOGOUT, REGISTER, GETUSERS };
 
 class Client : public QObject
 {
@@ -21,26 +23,27 @@ public:
     explicit Client(QObject *parent = 0);
     ~Client();
 
-    //Session(QString login, QString keyFileName);
     void sendRequest(Command cmd, QString request);
-    void createUserConnection(User user, QByteArray challenge/*, pk_context key*/);
-    void sendData(User user, QByteArray data);
-    bool isConnected();
+    bool createUserConnection(User *user);
+    bool sendData(QByteArray data);
+    bool isServerConnected();
+    bool isClientConnected();
     bool connectToServer();
     void setLogin(QString newLogin);
     QString getLogin();
     void setKeyFileName(QString newKeyFileName);
     QString getKeyFileName();
     void setReady(bool isReady) { ready = isReady; }
+    int getUsersCount();
 
 private:
-    QString login;
+    QString username;
     QString keyFile;
     bool ready;
     QList<User*> users;
     QTcpSocket *server;
-    QByteArray lastMessage;
 
+    void login(QByteArray);
     void getUserList(QByteArray);
     void registerUser(QByteArray);
 
@@ -49,11 +52,15 @@ signals:
     void messageReceived();
     void userListRecieved(QList<User*>);
     void serverDisconnected();
+    void displayMsg(QString, QString);
+    void loginSuccessful();
 
 public slots:
 
 private slots:
-    void readyRead();
+    void serverReadyRead();
+    void clientReadyRead();
+    void clientConnectionEstablished();
 };
 
 #endif // CLIENT_H
